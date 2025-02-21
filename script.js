93513624a6003c6f254c07a14ea328da05d6a290
Ammar2311 <ammardaoud668@gmail.com>
2025-02-21 17:52:05 +0300

    adding task desc field and table of done tasks

--- script.js
+++ script.js
@@ -5,12 +5,16 @@
 const start=document.getElementById("btn");  //target start downCount button
 
 
+
    start.addEventListener("click",function(e){        //starting down count when click button
 
+      const table=document.getElementById("study_table"); 
 
       const sec_field=document.getElementById("sec");            //target stopwatch
       const min_field=document.getElementById("min");
-      
+
+      const taskName=document.getElementById("task_field").value; //getting task descreption  
+
       let minutes=document.getElementById("minute").value;   //putting values in view box
       let hours=document.getElementById("hour").value;
 
@@ -24,12 +28,29 @@ const start=document.getElementById("btn");  //target start downCount button
       const myinterval2 =setInterval(downCountMin,60000);
       //every minute call downCountMin function
 
+     
+
+      let task=document.createElement("tr"); //create task
+
+      let time=document.createElement("td");
+      let desc=document.createElement("td");
+      time.innerHTML=minutes/60;      //asign values(time and descreption)
+      desc.innerHTML=taskName;
+
+
       function downCountSec(){
          if(sec_field.innerHTML==="0"){        //stop when you got 0       
+           
             clearInterval(myinterval);
+              
+            table.appendChild(task);   
+
+            task.appendChild(time);
+            task.appendChild(desc);
+
          }
         else
-        sec_field.innerHTML-=1;          //still decrease until 0
+          sec_field.innerHTML-=1;          //still decrease until 0
         }
 
         function downCountMin(){
@@ -53,20 +74,10 @@ const start=document.getElementById("btn");  //target start downCount button
      
         });
 
-
+          
 
    }
  );
 
 
-   // const stop=document.getElementById("btnStop");    //target cancel downCount buttosn
-   
-
-   // stop.addEventListener("click",function(e){
-
-        
-   //    clearInterval(myinterval);
-
-
-   // });
 
